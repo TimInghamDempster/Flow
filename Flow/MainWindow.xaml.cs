c68e5df2092c955e99b9b596e314b84a6a9b1f35
Tim Ingham-Dempster <tim_ingham_dempster@hotmail.co.uk>
2022-06-14 23:24:19 +0100

    Add project files.

---
+++ Flow/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Flow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+    }
+}
