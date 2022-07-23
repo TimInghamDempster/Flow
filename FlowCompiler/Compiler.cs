ead8976412bbfc4bd6d9962f7584759e1166ef3e
Tim Ingham-Dempster <tim_ingham_dempster@hotmail.co.uk>
2022-07-23 22:21:14 +0100

    Add a project for the compiler itself

---
+++ FlowCompiler/Compiler.cs
@@ -0,0 +1,15 @@
+﻿namespace FlowCompiler
+{
+    public interface ICompiler
+    {
+        string CompileLine(string line);
+    }
+
+    public class Compiler : ICompiler
+    {
+        public string CompileLine(string line)
+        {
+            return $" {line}";
+        }
+    }
+}
