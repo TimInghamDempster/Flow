a32fd6981e683da2ab6e611032617d165add19d1
Tim Ingham-Dempster <tim_ingham_dempster@hotmail.co.uk>
2022-11-11 22:12:27 +0000

    Consume the generated dll

--- Runtime/RuntimeAPI.cpp
+++ Runtime/RuntimeAPI.cpp
@@ -1,6 +1,25 @@
 #include "RuntimeAPI.h"
 
+#include <windows.h>
+
+typedef int(__stdcall* f_funci)();
+
 int Evaluate()
 {
-	return 13;
+    HINSTANCE hGetProcIDDLL = LoadLibrary(L"Content\\test.dll");
+
+    if (!hGetProcIDDLL) {
+        return -1;
+    }
+
+    f_funci funci = (f_funci)GetProcAddress(hGetProcIDDLL, "test_func");
+    if (!funci) {
+        return EXIT_FAILURE;
+    }
+
+    auto val = funci();
+
+    FreeLibrary(hGetProcIDDLL);
+
+    return val;
 }
