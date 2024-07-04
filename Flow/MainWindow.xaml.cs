9ee76c268171bdfa5f9205065c98567d5d491f77
Tim Ingham-Dempster <tim_ingham_dempster@hotmail.co.uk>
2024-07-04 23:48:23 +0100

    Remove the Razor project

--- Flow/MainWindow.xaml.cs
+++ Flow/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
 ﻿using System.Windows;
-using FlowCompiler;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Flow
 {
@@ -12,13 +10,6 @@ namespace Flow
         public MainWindow()
         {
             InitializeComponent();
-
-            var serviceCollection = new ServiceCollection();
-            serviceCollection.AddWpfBlazorWebView();
-            serviceCollection.AddSingleton<ICompiler, Compiler>();
-            serviceCollection.AddSingleton<IDebugger, Debugger>();
-            serviceCollection.AddSingleton<IBlockCompiler, BlockCompiler>();
-            Resources.Add("services", serviceCollection.BuildServiceProvider());
         }
     }
 }
