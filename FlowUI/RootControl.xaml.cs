23739c6b78931f2d63a89b3056e89a6d7388b55e
Tim Ingham-Dempster <tim_ingham_dempster@hotmail.co.uk>
2024-07-05 01:07:17 +0100

    Add a library for the WPF UI

---
+++ FlowUI/RootControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FlowUI
+{
+    /// <summary>
+    /// Interaction logic for RootControl.xaml
+    /// </summary>
+    public partial class RootControl : UserControl
+    {
+        public RootControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
